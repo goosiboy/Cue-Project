9150a837cfa97ef05eec901283ef1f5f028636cf
jonyoinas <jonyoinas@192.168.0.9>
2021-12-15 17:18:25 +0900

    Initial push

---
+++ cue-backend/src/main/java/com/github/oinasjo/CueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.oinasjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CueBackendApplication.class, args);
+	}
+
+}
---
+++ cue-backend/src/test/java/com/github/oinasjo/CueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oinasjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
